0f48fe6866542c57ba2307fdcd70c83b26314aa8
Himujjal <himu@tuta.io>
2021-07-01 01:33:10 +0530

    added url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
+const url = 'https://lebot-server-interview.herokuapp.com'
+
 // @ts-ignore
-export const backendurl = import.meta.env.BACKEND_URL || 'http://localhost:8080'
+export const backendurl = import.meta.env.DEV ? 'http://localhost:8080' : url
